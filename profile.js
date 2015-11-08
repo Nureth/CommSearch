959ab122f2ca12cf54d1e11df2b8ad1b90fe55e6
Hunter Chen <nureth100@gmail.com>
2015-11-08 09:55:35 -0500

    Add things

---
+++ profile.js
@@ -0,0 +1,7 @@
+var myDataRef = new Firebase('https://scorching-fire-6174.firebaseio.com/');
+var myUsers= myDataRef.child('users');
+var firstname = "";
+var lastname = "";
+var email = "";
+var password = "";
+var userInterestArray = [];
